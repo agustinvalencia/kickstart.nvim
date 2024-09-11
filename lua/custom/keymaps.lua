1c41ba9f3f97305ee600214f5817599042bb8ec8
agustin valencia <agustin.valencia@ericsson.com>
2024-09-11 09:54:33 +0200

    catppuccin and obsdidian added

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,3 +1,2 @@
 -- Oil
--- vim.keymap.set('n', '<leader>wf', '<cmd>Oil<cr>', { desc = '[W]orkspace [F]ile Explorer' })
 vim.keymap.set('n', '<leader>wf', require('oil').open_float, { desc = '[W]orkspace [F]ile Explorer' })
---
+++ lua/custom/plugins/obsidian-nvim.lua
@@ -0,0 +1,168 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- Required.
+    'hrsh7th/nvim-cmp', -- completion
+    'nvim-telescope/telescope.nvim', -- picker
+    'nvim-treesitter/nvim-treesitter', -- syntax highlighting
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'light-weight',
+        path = '/Users/eaguval/Library/Mobile Documents/iCloud~md~obsidian/Documents/lighweight-vault/',
+      },
+    },
+    open_app_foreground = true,
+    use_advanced_uri = true,
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+
+    daily_notes = {
+      folder = 'Journal/Daily',
+      default_tags = { 'daily' },
+      template = 'templates/daily.md',
+      date_format = '%Y-%m-%d',
+    },
+
+    templates = {
+      folder = 'templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {
+        now = function()
+          return os.date '%Y-%m-%d %H:%M'
+        end,
+        yesterday = function()
+          return os.date('%Y-%m-%d', os.time() - 86400)
+        end,
+        tomorrow = function()
+          return os.date('%Y-%m-%d', os.time() + 86400)
+        end,
+        today = function()
+          return os.date '%A'
+        end,
+      },
+    },
+
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = 'telescope.nvim',
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = '<C-x>',
+        -- Insert a link to the selected note.
+        insert_link = '<C-l>',
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = '<C-x>',
+        -- Insert a tag at the current location.
+        insert_tag = '<C-l>',
+      },
+    },
+
+    -- Optional, configure additional syntax highlighting / extmarks.
+    -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+    ui = {
+      enable = true, -- set to false to disable all additional syntax features
+      update_debounce = 200, -- update delay after a text change (in milliseconds)
+      max_file_length = 5000, -- disable UI features for files with more than this many lines
+      -- Define how various check-boxes are displayed
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        [' '] = { char = '󰄱 ', hl_group = 'ObsidianTodo' },
+        ['x'] = { char = ' ', hl_group = 'ObsidianDone' },
+        ['>'] = { char = ' ', hl_group = 'ObsidianRightArrow' },
+        ['~'] = { char = '󰰱 ', hl_group = 'ObsidianTilde' },
+        ['!'] = { char = ' ', hl_group = 'ObsidianImportant' },
+
+        -- You can also add more custom ones...
+      },
+      -- Use bullet marks for non-checkbox lists.
+      bullets = { char = '• ', hl_group = 'ObsidianBullet' },
+      external_link_icon = { char = ' ', hl_group = 'ObsidianExtLinkIcon' },
+      -- Replace the above with this if you don't have a patched font:
+      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      reference_text = { hl_group = 'ObsidianRefText' },
+      highlight_text = { hl_group = 'ObsidianHighlightText' },
+      tags = { hl_group = 'ObsidianTag' },
+      block_ids = { hl_group = 'ObsidianBlockID' },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+        ObsidianTodo = { bold = true, fg = '#f78c6c' },
+        ObsidianDone = { bold = true, fg = '#89ddff' },
+        ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+        ObsidianTilde = { bold = true, fg = '#ff5370' },
+        ObsidianImportant = { bold = true, fg = '#d73128' },
+        ObsidianBullet = { bold = true, fg = '#89ddff' },
+        ObsidianRefText = { underline = true, fg = '#c792ea' },
+        ObsidianExtLinkIcon = { fg = '#c792ea' },
+        ObsidianTag = { italic = true, fg = '#89ddff' },
+        ObsidianBlockID = { italic = true, fg = '#89ddff' },
+        ObsidianHighlightText = { bg = '#75662e' },
+      },
+    },
+
+    -- Specify how to handle attachments.
+    attachments = {
+      -- The default folder to place images in via `:ObsidianPasteImg`.
+      -- If this is a relative path it will be interpreted as relative to the vault root.
+      -- You can always override this per image by passing a full path to the command instead of just a filename.
+      img_folder = 'assets/imgs', -- This is the default
+
+      -- A function that determines the text to insert in the note when pasting an image.
+      -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+      -- This is the default implementation.
+      img_text_func = function(client, path)
+        path = client:vault_relative_path(path) or path
+        return string.format('![%s](%s)', path.name, path)
+      end,
+    },
+
+    --  * "use_alias_only", e.g. '[[Foo Bar]]'
+    --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+    --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+    --  * "use_path_only", e.g. '[[foo-bar.md]]'
+    -- Or you can set it to a function that takes a table of options and returns a string, like this:
+    wiki_link_func = 'prepend_note_path',
+
+    ---@param title string
+    ---@return string
+    note_id_func = function(title)
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+        return suffix
+      end
+      return title
+    end,
+
+    -- Optional, alternatively you can customize the frontmatter data.
+    ---@return table
+    note_frontmatter_func = function(note)
+      local out = { id = note.id, tags = note.tags }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
+  },
+}
