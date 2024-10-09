27e74747bef827f0c451e0280ddb22137040bd5d
agustin valencia <agustin.valencia@ericsson.com>
2024-10-09 13:28:10 +0200

    update on image-nvim

--- lua/custom/plugins/image-nvim.lua
+++ lua/custom/plugins/image-nvim.lua
@@ -16,6 +16,7 @@ return {
     max_height = nil,
     max_width_window_percentage = nil,
     max_height_window_percentage = 50,
+    tmux_show_only_in_active_window = true,
     integrations = {
       markdown = {
         enabled = true,
@@ -24,6 +25,9 @@ return {
         only_render_image_at_cursor = false,
         filetypes = { 'markdown' }, -- markdown extensions (ie. quarto) can go here
       },
+      neorg = { enabled = false },
+      html = { enabled = false },
+      css = { enabled = false },
     },
   },
 }
